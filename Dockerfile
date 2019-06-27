fc44324a107ab4abedb2a8207b0d5da66a6032d1
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-06-27 17:40:42 +0200

    wip add usb actions

--- Dockerfile
+++ Dockerfile
@@ -14,11 +14,23 @@ LABEL git.build.time=${GIT_BUILD_TIME}
 LABEL travis.build.number=${TRAVIS_BUILD_NUMBER}
 LABEL travis.build.web.url=${TRAVIS_BUILD_WEB_URL}
 
-RUN apt update && apt-get install -y --no-install-recommends usbutils=1:007-4+b1 curl=7.52.1-5+deb9u9 udev=232-25+deb9u11 jq=1.5+dfsg-1.3
+RUN apt update && apt-get install -y --no-install-recommends \
+                    usbutils=1:007-4+b1 \
+                    curl=7.52.1-5+deb9u9 \
+                    udev=232-25+deb9u11 \
+                    jq=1.5+dfsg-1.3 \
+                    inotify-tools=3.14-2
 
 RUN apt-get clean autoclean \
         && apt-get autoremove --yes \
         && /bin/bash -c "rm -rf /var/lib/{apt,dpkg,cache,log}/"debian:stretch-slim
+
+# Another way to do this (more complex but more powerful as well) is to install systemd
+# inside the Docker image and move the /dev mount into a tmpmount inside the container
+# See Balena's example: https://github.com/balena-io-library/base-images/blob/master/balena-base-images/armv7hf/debian/stretch/run/entry.sh
+
+
+
 
 COPY code/ /opt/nuvlabox/
 
--- code/app.sh
+++ code/app.sh
@@ -1,5 +1,64 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+header_message="NuvlaBox Peripheral Manager service for USB devices
+\n\n
+This microservice is responsible for the autodiscovery and classification
+of USB devices in the NuvlaBox.
+\n\n
+Whenever a USB peripheral is added or removed from the NuvlaBox, this
+microservice will find its properties and report it to Nuvla.
+\n\n
+Arguments:\n
+  No arguments are expected.\n
+  This message will be shown whenever -h, --help or help is provided and a
+  command to the Docker container.\n
+"
 
 set -e
 
-udevadm monitor
+SOME_ARG="$1"
+
+help_info() {
+    echo "COMMAND: ${1}. You have asked for help:"
+    echo -e ${header_message}
+    exit 0
+}
+
+if [[ ! -z ${SOME_ARG} ]]
+then
+    if [[ "${SOME_ARG}" = "-h" ]] || [[ "${SOME_ARG}" = "--help" ]] || [[ "${SOME_ARG}" = "help" ]]
+    then
+        help_info ${SOME_ARG}
+    else
+        echo "WARNING: this container does not expect any arguments, thus they'll be ignored"
+    fi
+fi
+
+echo "INFO: start listening for USB related events..."
+
+# Using inotify instead of udev
+# To use udev, please check the Dockerfile for an implementation reference with systemd-udev
+
+pipefail=$(date +%s)
+
+mkfifo ${pipefail}
+inotifywait -m -q -r /dev/bus/usb -e CREATE -e DELETE --csv > ${pipefail}
+while read event
+do
+    echo ${event}
+    devnumber=$(echo ${event} | awk -F',' '{print $NF}')
+    buspath=$(echo ${event} | awk -F',' '{print $1}')
+    action=$(echo ${event} | awk -F',' '{print $2}')
+
+    if [[ "${action}" = "CREATE" ]]
+    then
+        echo "INFO: creating USB peripheral in Nuvla"
+        nuvlabox-add-usb-peripheral ${buspath} ${devnumber}
+    fi
+
+    if [[ "${action}" = "DELETE" ]]
+    then
+        echo "INFO: deleting USB peripheral from Nuvla"
+        nuvlabox-delete-usb-peripheral ${buspath} ${devnumber}
+    fi
+done < ${pipefail}
